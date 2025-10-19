3f50adab2ec42f49771c648a8320ba068b45a758
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-10-19 21:05:45 +0100

    current progres 1

--- String Functions/ft_strnstr.c
+++ String Functions/ft_strnstr.c
@@ -6,11 +6,40 @@
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 20:28:50 by alamrani          #+#    #+#             */
-/*   Updated: 2025/10/19 20:30:44 by alamrani         ###   ########.fr       */
+/*   Updated: 2025/10/19 21:03:49 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stddef.h>
 
-char	*strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	i;
+	size_t	j;
 	
+	i = 0;
+	j = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		while (big[i + j] == little[j] && little[j] && i + j < len)
+		{
+			j++;
+		}
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+		j = 0;
+	}
+	return (NULL);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int	main()
+{
+	const char *big =  "hello hamid, i know you have the potential, i really do";
+	const char *ltl = "";
+	const char *result = ft_strnstr(big, ltl, 7);
+	printf("%s\n",result);
+} */
