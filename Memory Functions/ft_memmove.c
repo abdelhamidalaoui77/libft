8aa3fd81c16d6fe8d8eab77b87233c93902bdaf9
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-10-20 22:06:55 +0100

    adding memmove

--- Memory Functions/ft_memmove.c
+++ Memory Functions/ft_memmove.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/20 16:18:10 by alamrani          #+#    #+#             */
-/*   Updated: 2025/10/20 21:59:36 by alamrani         ###   ########.fr       */
+/*   Created: 2025/10/20 20:44:21 by alamrani          #+#    #+#             */
+/*   Updated: 2025/10/20 22:06:24 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-
+/*
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned const char	*s;
@@ -28,8 +28,34 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		i++;
 	}
 	return (d);
+} */
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned const char	*s;
+	unsigned char		*d;
+	size_t				i;
+
+	if (!dest && !src)
+		return (NULL);
+	s = (unsigned const char *)src;
+	d = (unsigned char *)dest;
+	i = 0;
+	if (d > s)
+	{
+		while (n--)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		ft_memcpy(d, s, n);
+	}
+	return (dest);
 }
 /*
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,7 +64,7 @@ int	main()
 {
 	const char *src = "Hello, ahmed akala tofa7atan";
 	char	dst[28];
-	memcpy(dst, src, 20);
+	ft_memmove(dst, src, 28);
 	printf("%s\n",dst);
-	printf("%zu\n",sizeof(src));	
+	printf("%zu\n",sizeof(src));
 } */
