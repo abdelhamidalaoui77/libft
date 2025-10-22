b1c8df59d5d45a3d7003fc588fe37fe488e4dc19
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-10-22 21:11:03 +0100

    until the orale

--- Additional Functions/ft_strtrim.c
+++ Additional Functions/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 11:50:34 by alamrani          #+#    #+#             */
-/*   Updated: 2025/10/22 12:29:33 by alamrani         ###   ########.fr       */
+/*   Updated: 2025/10/22 20:42:42 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -23,20 +23,62 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-int	set_occurrence(char const *s1, char const *set)
+int	in_set(char c, const char *set)
 {
-	int	i;
-	int	j;
-	int	total_occurrence;
-
-	i = 0;
-	total_occurrence = 0;
-	while (s1[i])
+	while (*set)
 	{
-		
+		if (c == *set)
+			return (1);
+		set++;
 	}
+	return (0);
 }
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	start;
+	size_t	end;
+	size_t	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && in_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && in_set(s1[end - 1], set))
+		end--;
+	len = end - start;
+	return (ft_substr(s1, start, len));
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *s1 = "   !!!42 Network!!!   ";
+	char *set = " !";
+	char *res = ft_strtrim(s1, set);
+
+	printf("Before : \"%s\"\n", s1);
+	printf("After  : \"%s\"\n", res);
+	free(res);
+} */
