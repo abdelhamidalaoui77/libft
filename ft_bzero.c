8d04193a7981a0c3e4d905aa903f7217d3838206
abdelhamidalaoui77 <abdelhamidalaoui77@gmail.com>
2025-11-02 23:22:19 +0100

    working on libft before pushing

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,17 +6,17 @@
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 19:14:01 by alamrani          #+#    #+#             */
-/*   Updated: 2025/10/19 13:34:32 by alamrani         ###   ########.fr       */
+/*   Updated: 2025/11/02 23:21:39 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t			i;
-	unsigned char	*ptr;
+	size_t	i;
+	char	*ptr;
 
-	ptr = (unsigned char *)s;
+	ptr = (char *)s;
 	i = 0;
 	while (i < n)
 	{
@@ -24,29 +24,26 @@ void	ft_bzero(void *s, size_t n)
 		i++;
 	}
 }
-
+/*
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <bsd/string.h>
 
-// int	main()
-// {
-// 	// char str[] = "aaaaaaaaabzero";
-// 	// size_t i = 9;
-// 	// char *str = malloc(15 + 1);
-// 	// if (!str)
-// 	// 	return 1;
-// 	// bzero(str,i);
-// 	// printf("%s",str+8);
-// 	// free(str);
-// 	// return 0;
-// 	int i = 1564257;
-// 	bzero((unsigned char *)&i + 1, 3);
-// 	printf("%d\n", i);
-// }
-
-
-
+int	main()
+{
+	// char str[] = "aaaaaaaaabzero";
+	// size_t i = 9;
+	// char *str = malloc(15 + 1);
+	// if (!str)
+	// 	return 1;
+	// bzero(str,i);
+	// printf("%s",str+8);
+	// free(str);
+	// return 0;
+	int i = 257;
+	ft_bzero((unsigned char *)&i + 1, 3);
+	printf("%d\n", i);
+} */
 //257 -> 1 ft_bzero
--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: alamrani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 19:07:45 by alamrani          #+#    #+#             */
-/*   Updated: 2025/10/19 14:17:33 by alamrani         ###   ########.fr       */
+/*   Updated: 2025/11/02 23:17:15 by alamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -26,13 +26,13 @@ void	*ft_memset(void *s, int c, size_t n)
 	return (s);
 }
 /*
+#include <stdio.h>
 int main()
 {
 	int a;
-	void *y = &a;
-	ft_memset(y,57,1);
-	ft_memset(y,5,1);
-	ft_memset(y,0,0);
-	ft_memset(y,0,0);
-	printf("%s",(char*)y);
-} */
+
+	ft_memset((char *)&a, 57, 1);
+	ft_memset((char *)&a+1, 5, 1);
+	
+	printf("%d\n",a);
+}*/
